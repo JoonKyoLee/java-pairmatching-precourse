3b9d61e309041d69781af3cae5a79f0ef36a1661
JoonKyo <joonkyolee@gmail.com>
2023-12-15 17:09:02 +0900

    feat(Matches): 선택한 코스, 레벨, 미션에서 페어 매칭이 존재하는 지 판단하는 메서드 추가

--- src/main/java/pairmatching/Matches.java
+++ src/main/java/pairmatching/Matches.java
@@ -1,2 +1,22 @@
-package pairmatching;public class Matches {
+package pairmatching;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Matches {
+    private Map<String, List<List<String>>> matches;
+
+    public Matches() {
+        matches = new HashMap<>();
+    }
+
+    public List<List<String>> findExistedMatchingResult(String course, String level, String mission) {
+        String keyIndex = new Match(course, level, mission).getKeyIndex();
+
+        if (matches.containsKey(keyIndex)) {
+            return matches.get(keyIndex);
+        }
+        return null;
+    }
 }
