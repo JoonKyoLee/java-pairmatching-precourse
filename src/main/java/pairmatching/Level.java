d69684e420309b6ec90c3f9adebe2130af0151a2
JoonKyo <joonkyolee@gmail.com>
2023-12-15 15:57:18 +0900

    feat(Level): 각 레벨을 enum으로 저장 및 레벨 이름으로 레벨을 찾을 수 있는 메서드 추가

--- src/main/java/pairmatching/Level.java
+++ src/main/java/pairmatching/Level.java
@@ -1,2 +1,41 @@
-package pairmatching;public class Level {
+package pairmatching;
+
+import java.util.Arrays;
+
+public enum Level {
+    NONE("없음", 0, 0),
+    LEVEL_1("레벨1", 1, 3),
+    LEVEL_2("레벨2", 2, 3),
+    LEVEL_3("레벨3", 3, 0),
+    LEVEL_4("레벨4", 4, 2),
+    LEVEL_5("레벨5", 5, 0);
+
+    private final String levelName;
+    private final int index;
+    private final int missionNum;
+
+    Level(String levelName, int index, int missionNum) {
+        this.levelName = levelName;
+        this.index = index;
+        this.missionNum = missionNum;
+    }
+
+    public String getLevelName() {
+        return levelName;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getMissionNum() {
+        return missionNum;
+    }
+
+    public static Level getLevel(String levelName) {
+        return Arrays.stream(values())
+                .filter(level -> level.levelName.equals(levelName))
+                .findFirst()
+                .orElse(NONE);
+    }
 }
