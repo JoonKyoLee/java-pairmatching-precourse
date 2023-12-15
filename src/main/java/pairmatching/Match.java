193ff392cdf8e4b78a6c74b4d68f725031d821ac
JoonKyo <joonkyolee@gmail.com>
2023-12-15 16:55:07 +0900

    feat(Match): 입력받은 문자열을 토대로 과정, 레벨, 미션을 찾고 Map에서 사용할 키 값 생성하는 메서드 추가

--- src/main/java/pairmatching/Match.java
+++ src/main/java/pairmatching/Match.java
@@ -1,13 +1,53 @@
 package pairmatching;
 
-public class Matching {
-    private final String course;
-    private final String level;
-    private final String mission;
+public class Match {
+    private static final int MISSION_NUM_BOUND = 0;
+    private final Course course;
+    private final Level level;
+    private final Mission mission;
+    private final String keyIndex;
 
-    public Matching(String course, String level, String mission) {
-        this.course = course;
-        this.level = level;
-        this.mission = mission;
+    public Match(String course, String level, String mission) {
+        this.course = Course.getCourse(course);
+        this.level = Level.getLevel(level);
+        this.mission = allocateMission(mission);
+
+        validate();
+
+        keyIndex = makeKeyIndex();
+    }
+
+    private Mission allocateMission(String mission) {
+        if (levelHasMission()) {
+            return Mission.getMission(mission);
+        }
+        return null;
+    }
+
+    private boolean levelHasMission() {
+        return this.level.getMissionNum() > MISSION_NUM_BOUND;
+    }
+
+    private void validate() {
+        if (missionIsNotInLevel()) {
+            throw new InputException(ErrorMessage.LEVEL_AND_MISSION_DOES_NOT_MATCH);
+        }
+    }
+
+    private boolean missionIsNotInLevel() {
+        return level != mission.getLevel();
+    }
+
+    private String makeKeyIndex() {
+        String key = course.getIndex() + level.getIndex();
+
+        if (levelHasMission()) {
+            return key + mission.getIndex();
+        }
+        return key;
+    }
+
+    public String getKeyIndex() {
+        return keyIndex;
     }
 }
